146b3d9deb64e110defb3c068b45ae7ead991599
JosueQB <jquijije2001@gmail.com>
2024-06-10 21:01:37 -0500

    se agrega

--- src/app/reciente/page.js
+++ src/app/reciente/page.js
@@ -22,13 +22,13 @@ export default function recientes() {
         <div>
           <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. <br /><br /> Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. <br /><br /> No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
         </div>
-        <div>
+        <div className={styles.img}>
           <Image src='/static/img/galaxy-planets-solar.png' height={400} width={400} alt='solar' />
         </div>
         <div>
           <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. <br /><br />No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
         </div>
-        <div>
+        <div className={styles.img}>
           <Image src='/static/img/venus_galaxy_planet.png' height={300} width={300} alt='venus' />
         </div>
       </div>
